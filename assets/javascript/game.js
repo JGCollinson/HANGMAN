f9e9f89fe78e37fe4f88e84fbb282ada7aea7e63
GarrettCollinson <jgarrettcollinson@outlook.com>
2018-03-02 23:35:23 -0500

    finished

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,60 +1,25 @@
- // This function is run whenever the user presses a key.
- document.onkeyup = function(event) {
+// Creates an array that lists out all of the options (Rock, Paper, or Scissors).
+//PRESS ANY KEY TO START
+var spacesReset1 = ["_", "_", "_", "_"];
+var spaces = ["_", "_", "_", "_"];
+var answers1 = ["l", "u", "n", "a"];
+var notThese = [];
+var numboGuess = 15;
+var correctCounter = 0;
+var numwins = 0;
 
-    // Determines which key was pressed.
-    var userGuess = event.key;
-
-    if (["r", "e", "g","o","l","i","t","h"].includes(userGuess)) {
-     else if (userGuess === 'a') 
-        wins++;
-    }
-
-      // Randomly chooses a choice from the options array. This is the Computer's guess.
-
-      // Alerts the key the user pressed (userGuess).
-      console.log("User guess: " + userGuess);
-
-              //https://en.wikipedia.org/wiki/Selene
-              //albedo
-              //https://en.wikipedia.org/wiki/Lunar_Roving_Vehicle
-              //perigee
-              //http://www.spudislunarresources.com/moon101/moon_101_intro.pdf
-              //https://gizmodo.com/scientists-just-found-the-perfect-spot-to-build-an-unde-1819658831
-
-      if (userGuess === computerGuess) {
-        ties++;
-      } else if (userGuess === 'a') {
-        wins++;
-      } else if (userGuess === 'p' && computerGuess === 'r') {
-        wins++;
-      } else if (userGuess === 's' && computerGuess === 'p') {
-        wins++;
-      } else {
-        losses++;
-      }
-
-              
-
-      gameStatus.innerHTML = `
-      <p>You chose: ${userGuess}</p>
-      <p>Computer chose: ${computerGuess}</p>
-      <p>wins: ${wins}</p>
-      <p>losses: ${losses}</p>
-      <p>ties: ${ties}</p>
-      `;}
-
-// We can either overwrite each element of the array to make it lowercase, or we can use the toLowerCase() method.
-
-
-logArray(drinks) //-> Logs array
-
-      
-
-// If the first character in the current animal is "c" or "o", alert the following message.
-if (myFarm[j].charAt(0) === "c" || myFarm[j].charAt(0) === "o") {
-        alert("Starts with a c or an o!");
-      }
-    }
-
-  };
-</script>
+function updateSpaces() {
+document.getElementById("game").innerHTML = spaces.join(" ");
+}
+function updateGuessesLeft() {
+document.querySelector("#numguesses").innerHTML = numboGuess;
+}
+function updateLettersGuessed() {	
+document.querySelector("#numbertime").innerHTML = notThese.toString();
+}
+function updateWins() {	
+document.querySelector("#wins").innerHTML = "Wins:" + numwins;
+}
+function updateTrivia() {	
+document.querySelector("#picText").innerHTML = "The Roman Goddess of the Moon: Luna is described as having horns and a chariot in continuous chase with Sol (the Sun), \"all-seeing\", \"all-wise\", a lover of horses, vigilance, and a \"foe of strife\" who \"giv\'st to Nature\'s works their destin\'d end\".";
+}
